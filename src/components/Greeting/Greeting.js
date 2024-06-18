e99be599e3aa43c7c8beb18d25f7f898cb017256
Sergey Kuznetsov <SKuznetsov3@alfabank.ru>
2024-06-18 14:01:50 +0300

    Удалено ненужное

--- src/components/Greeting/Greeting.js
+++
@@ -1,9 +0,0 @@
-export const Greeting = () => (
-  <>
-    <h1>Hello world!</h1>
-    <h2 data-testid="description-elem">It is a testing component</h2>
-    <button>Click me!</button>
-    <br />
-    <input type="text" placeholder="input value..." />
-  </>
-);
--- src/pages/main/MainPage.js
+++ src/pages/main/MainPage.js
@@ -1,18 +1,10 @@
-import { AsyncComponent } from "../../components/AsyncComponent/AsyncComponent";
 import { Counter } from "../../components/Counter/Counter";
-import { Events } from "../../components/Events/Events";
-import { Fetching } from "../../components/Fetching/Fetching";
-import { Greeting } from "../../components/Greeting/Greeting";
 import cls from "./MainPage.module.css";
 
 function MainPage() {
   return (
     <div className={cls.MainPage}>
-      {/* <Greeting /> */}
-      {/* <AsyncComponent /> */}
-      {/* <Events /> */}
-      <Fetching />
-      {/* <Counter /> */}
+      <Counter />
     </div>
   );
 }
